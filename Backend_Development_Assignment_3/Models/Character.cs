211c3daef8b364adbe0128a0a0d7f02447cb96f6
richardstolen <richstolen@gmail.com>
2023-01-30 13:10:41 +0100

    :sparkles: Added Character class and Gender enum

---
+++ Backend_Development_Assignment_3/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend_Development_Assignment_3.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Alias { get; set; }
+        public Gender Gender { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
---
+++ Backend_Development_Assignment_3/Models/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend_Development_Assignment_3.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
