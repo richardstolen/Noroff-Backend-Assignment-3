eb731aafc8159de9074c0e2853f85d95dcb99f4b
richardstolen <richstolen@gmail.com>
2023-01-30 13:12:40 +0100

    :sparkles: Added Movie class

--- Backend_Development_Assignment_3/Models/Character.cs
+++ Backend_Development_Assignment_3/Models/Character.cs
@@ -3,9 +3,9 @@
     public class Character
     {
         public int Id { get; set; }
-        public string FullName { get; set; }
-        public string Alias { get; set; }
+        public string? FullName { get; set; }
+        public string? Alias { get; set; }
         public Gender Gender { get; set; }
-        public string PictureUrl { get; set; }
+        public string? PictureUrl { get; set; }
     }
 }
---
+++ Backend_Development_Assignment_3/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend_Development_Assignment_3.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public string? Director { get; set; }
+        public string? PictureUrl { get; set; }
+        public string? TrailerUrl { get; set; }
+    }
+}
